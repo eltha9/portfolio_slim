37dbfff6b33e0fec67d6c03ccf80be7009af2a56
eltha9 <theadosto@gmail.com>
2019-06-01 08:47:15 +0200

    update route en and services

--- app/services.php
+++ app/services.php
@@ -28,31 +28,51 @@ $container['db'] = function($container){
 
 
 // MAILER SERVICE 
-/*
-//transport 1 
-$transport = (new Swift_SmtpTransport('smtp.example.org', 25))
-  ->setUsername('your username')
-  ->setPassword('your password')
 
+// $container['mailer'] = function($container){
+//   // transport 
+//   $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+//   ->setUsername('santosphilippe93@gmail.com')
+//   ->setPassword('zth18ap4');
+
+//   $mailer = new Swift_Mailer($transport);
+
+//   $message = (new Swift_Message('Wonderful Subject'));
+
+
+//   return $message;
+//   // ->setFrom(['elph@elph.com' => 'elph'])
+//   // ->setTo(['santosphilippe93@gmail.com'])
+//   // ->setBody('Here is the message itself')
+//   // ;
+
+//   // // Send the message
+//   // $result = $mailer->send($message);
+// };
+
+
+//transport 1 
+// $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+//   ->setUsername('santosphilippe93@gmail.com')
+//   ->setPassword('zth18ap4');
 
   //transport 2
 
-$transport = new Swift_SendmailTransport('/usr/sbin/sendmail -bs');
+// $transport = new Swift_SendmailTransport('/usr/sbin/sendmail -bs');
 
 
 
 
-// Create the Mailer using your created Transport
-$mailer = new Swift_Mailer($transport);
+// // Create the Mailer using your created Transport
+// $mailer = new Swift_Mailer($transport);
 
 
-// Create a message
-$message = (new Swift_Message('Wonderful Subject'))
-  ->setFrom(['john@doe.com' => 'John Doe'])
-  ->setTo(['santosphilippe93@gmail.com'])
-  ->setBody('Here is the message itself')
-  ;
+// // Create a message
+// $message = (new Swift_Message('Wonderful Subject'))
+//   ->setFrom(['elph@elph.com' => 'elph'])
+//   ->setTo(['santosphilippe93@gmail.com'])
+//   ->setBody('Here is the message itself')
+//   ;
 
-// Send the message
-$result = $mailer->send($message);
-*/
+// // Send the message
+// $result = $mailer->send($message);
